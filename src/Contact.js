3764f297b4e3cf1b91003edc13415222478b0490
tushshah10 <tushshah10@gmail.com>
2022-06-17 09:54:34 +0530

    commit4

--- src/Contact.js
+++ src/Contact.js
@@ -9,6 +9,8 @@ function Contact()
             <li><p><a href="https://www.linkedin.com/in/tusharshah10/">LinkedIn</a> -Tushar Shah</p></li>
             <li><p><a href="https://www.instagram.com/tusharshah10/">Instagram</a> @tusharshah10</p></li>
             <li><p><a href="https://twitter.com/TushShah10">Twitter</a> @tushshah10</p></li>
+            <li><p><a href="https://medium.com/@tushshah10">Medium</a> @tushshah10</p></li>
+            <li><p><a href="https://github.com/tushshah10">GitHub</a> @tushshah10</p></li>
             <li><p><ButtonMailto label="Write me an E-Mail" mailto="mailto:tushshah10@gmail.com" /></p></li>
             <li><p>Phone- +91 8939019360</p></li>
             
